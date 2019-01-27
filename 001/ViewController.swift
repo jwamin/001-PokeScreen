cfd4d26e1df56ce53f734547a41b910a85604d32
Joss Manger <Jwam3rd@gmail.com>
2019-01-27 16:36:11 -0500

    Initial Commit

---
+++ 001/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  001
+//
+//  Created by Joss Manger on 1/27/19.
+//  Copyright © 2019 Joss Manger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
